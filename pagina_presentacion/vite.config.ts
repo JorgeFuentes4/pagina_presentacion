17be5754ffcae6ff516e68421cf3258186108c68
Jorge Ignacio Fuentes Jara <113200270+jorgefuentes45@users.noreply.github.com>
2026-09-03 17:29:57 -0400

    otro cambio

--- pagina_presentacion/vite.config.ts
+++ pagina_presentacion/vite.config.ts
@@ -4,5 +4,5 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/pagina_presentacion/',
+    base: '/pagina_presentacion/',
 })
